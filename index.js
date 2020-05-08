84672b2f50e21b6a406f3459186eca4a0d390ede
Lara Petersen <laramarlenpetersen@gmail.com>
2020-05-08 16:58:28 +0200

    created hover effects on project cards

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName("nav").style.display = "none";
+
+var openmenu = document.getElementsByClassName("burger-menu");
+var closemenu = document.getElementsByClassName("close");
+// openmenu.onclick = function openmenu() {
+//   document.getElementsByTagName(nav).show();
+// };
